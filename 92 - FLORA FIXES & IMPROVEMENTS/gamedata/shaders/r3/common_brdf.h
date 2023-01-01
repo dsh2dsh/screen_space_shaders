da9adb570030b2045b078a8617dd87682a219042
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2023-01-01 14:59:24 +0100

    Screen Space Shaders 13.2
    https://www.moddb.com/mods/stalker-anomaly/addons/screen-space-shaders
    
    UPDATE 13.2 [ 2022-12-28 ]
    
    * New Fog Shader
    
      * Mix of Height fog and Distance fog.
    
      * The sun color affect the fog.
    
      * Performance friendly.
    
      * Fixed a shader without fog implementation.

---
+++ 92 - FLORA FIXES & IMPROVEMENTS/gamedata/shaders/r3/common_brdf.h
@@ -0,0 +1,21 @@
+/*
+	Common functions used by lighting 
+	Material table
+	
+	/////////////////
+	Anomaly Team 2020
+	/////////////////
+*/
+#include "common.h"
+
+////////////////////////
+//Material table
+#define MAT_FLORA 0.15f
+
+// Simple subsurface scattering
+float SSS(float3 N, float3 V, float3 L)
+{
+	float intensity = 0.5f;
+	float S = saturate(dot(V, -(L + N))) * intensity;
+	return S;
+}
